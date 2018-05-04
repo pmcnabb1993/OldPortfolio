59bd3ffbc33f798a2bd5ffc3ccbb75b4ca3c62e9
Preston McNabb <prestonmcnabb22@gmail.com>
2018-05-04 17:37:00 -0500

    added projects and images

--- PrestonPortfolio/src/pages/index.js
+++ PrestonPortfolio/src/pages/index.js
@@ -12,7 +12,16 @@ import Nav from '../components/Nav'
 import pic01 from '../assets/images/pic01.jpg'
 
 import weatherapp from '../assets/images/weatherapp.png'
-
+import LIRI from '../assets/images/LIRI.png'
+import MetroTracker from '../assets/images/MetroTracker.png'
+import giphy from '../assets/images/giphy.png'
+import TypeSearch from '../assets/images/TypeSearch.png'
+import BurgerApp from '../assets/images/BurgerApp.png'
+import MarioApp from '../assets/images/MarioApp.png'
+import Dolo from '../assets/images/Dolo.png'
+import FriendFinder from '../assets/images/LiveAthlete.png'
+import LiveAthlete from '../assets/images/LiveAthlete.png'
+import StrangerTrivia from '../assets/images/StrangerTrivia.png'
 
 class Index extends React.Component {
   constructor(props) {
@@ -75,22 +84,110 @@ class Index extends React.Component {
             <ul className="features">
               <li>
               <Project
-                  src={weatherapp}
+                  src={Dolo}
                   colour="#FFFFFF"
                   title="Weather App"
-                  link="https://www.littlenomad.co/"
+                  link="http://www.dolo.io/"
                   timeperiod="2017"
                   subtitle="A platform for socially aware travellers. Runs on
                     React+Redux with a Ruby on Rails backend."
                 /> 
               </li>
-
+              <li>
+              <Project
+                  src={LiveAthlete}
+                  colour="#FFFFFF"
+                  title="Weather App"
+                  link="https://gitdarren.github.io/LiveAthle…"
+                  timeperiod="2017"
+                  subtitle="A platform for socially aware travellers. Runs on
+                    React+Redux with a Ruby on Rails backend."
+                /> 
+              </li>
               <li>
               <Project
                   src={weatherapp}
                   colour="#FFFFFF"
                   title="Weather App"
                   link="https://preston-weather-app.herokuapp.com/"
+                  timeperiod="2017"
+                  subtitle="A platform for socially aware travellers. Runs on
+                    React+Redux with a Ruby on Rails backend."
+                />
+              </li>
+
+              <li>
+              <Project
+                  src={MetroTracker}
+                  colour="#FFFFFF"
+                  title="Weather App"
+                  link="https://preston-weather-app.herokuapp.com/"
+                  timeperiod="2017"
+                  subtitle="A platform for socially aware travellers. Runs on
+                    React+Redux with a Ruby on Rails backend."
+                />
+              </li>
+              <li>
+              <Project
+                  src={LIRI}
+                  colour="#FFFFFF"
+                  title="Weather App"
+                  link="https://preston-weather-app.herokuapp.com/"
+                  timeperiod="2017"
+                  subtitle="A platform for socially aware travellers. Runs on
+                    React+Redux with a Ruby on Rails backend."
+                />
+              </li>
+              <li>
+              <Project
+                  src={BurgerApp}
+                  colour="#FFFFFF"
+                  title="Weather App"
+                  link=" https://preston-burger-app.herokuapp.com/"
+                  timeperiod="2017"
+                  subtitle="A platform for socially aware travellers. Runs on
+                    React+Redux with a Ruby on Rails backend."
+                />
+              </li>
+              <li>
+              <Project
+                  src={StrangerTrivia}
+                  colour="#FFFFFF"
+                  title="Weather App"
+                  link="https://pmcnabb1993.github.io/Trivia_Game/"
+                  timeperiod="2017"
+                  subtitle="A platform for socially aware travellers. Runs on
+                    React+Redux with a Ruby on Rails backend."
+                />
+              </li>
+              <li>
+              <Project
+                  src={TypeSearch}
+                  colour="#FFFFFF"
+                  title="Weather App"
+                  link="https://preston-weather-app.herokuapp.com/"
+                  timeperiod="2017"
+                  subtitle="A platform for socially aware travellers. Runs on
+                    React+Redux with a Ruby on Rails backend."
+                />
+              </li>
+              <li>
+              <Project
+                  src={FriendFinder}
+                  colour="#FFFFFF"
+                  title="Weather App"
+                  link="https://preston-friend-finder.herokuapp.com/home.html"
+                  timeperiod="2017"
+                  subtitle="A platform for socially aware travellers. Runs on
+                    React+Redux with a Ruby on Rails backend."
+                />
+              </li>
+              <li>
+              <Project
+                  src={MarioApp}
+                  colour="#FFFFFF"
+                  title="Weather App"
+                  link="https://mario-memory-game.herokuapp.com/"
                   timeperiod="2017"
                   subtitle="A platform for socially aware travellers. Runs on
                     React+Redux with a Ruby on Rails backend."
@@ -109,38 +206,6 @@ class Index extends React.Component {
               </li>
               <li>
               
-              </li>
-            </ul>
-            <footer className="major">
-            </footer>
-          </section>
-
-          <section id="second" className="main special">
-            <header className="major">
-              <h2>Work Experience</h2>
-              <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
-              posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
-            </header>
-            <ul className="statistics">
-              <li className="style1">
-                <span className="icon fa-code-fork"></span>
-                <strong>5,120</strong> Etiam
-              </li>
-              <li className="style2">
-                <span className="icon fa-folder-open-o"></span>
-                <strong>8,192</strong> Magna
-              </li>
-              <li className="style3">
-                <span className="icon fa-signal"></span>
-                <strong>2,048</strong> Tempus
-              </li>
-              <li className="style4">
-                <span className="icon fa-laptop"></span>
-                <strong>4,096</strong> Aliquam
-              </li>
-              <li className="style5">
-                <span className="icon fa-diamond"></span>
-                <strong>1,024</strong> Nullam
               </li>
             </ul>
             <p className="content">Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum ac arcu sit amet, fermentum pellentesque et purus. Integer maximus varius lorem, sed convallis diam accumsan sed. Etiam porttitor placerat sapien, sed eleifend a enim pulvinar faucibus semper quis ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer eget purus nec nulla mattis et accumsan ut magna libero. Morbi auctor iaculis porttitor. Sed ut magna ac risus et hendrerit scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras eu ornare dui curabitur lacinia.</p>
